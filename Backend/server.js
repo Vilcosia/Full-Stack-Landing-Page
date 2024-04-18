01a5b9cb295e71c4e628843594f6853c61b0ca44
Koketso Mosefowa <vilcosia@gmail.com>
2024-04-18 22:08:34 +0200

    new

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var cors = require('cors');
+require("dotenv").config();
+const db = require("./Config/db");
+const app = express();
+
+//impot classes
+//const routes = require("./routes/routes");
+// const userRoute = require("./routes/userRoute");
+// const clientRoute = require("./routes/clientRoute");
+// const invoiceRoute = require("./routes/invoiceRoute");
+
+var corsOptions = {
+    origin: "*"
+  };
+  app.use(cors(corsOptions));
+
+
+//starting the servers
+app.use(cors());
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.listen(8080,() => {console.log('Server running on port 8080');});
+
+if(db)
+{
+    console.log("Database is connected");
+}
+// app.use('/api/user', userRoute);
+// app.use('/api/client',clientRoute);
+// app.use('/api/invoice',invoiceRoute);
