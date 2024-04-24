97b60e0fbf812294b71d23e1df62b004d3e8c6fc
Koketso Mosefowa <vilcosia@gmail.com>
2024-04-24 12:30:42 +0200

    final

--- Backend/Controllers/register_login.js
+++ Backend/Controllers/register_login.js
@@ -1,17 +1,10 @@
-//const db = require("../Config/db");
+const db = require("../Config/db");
 const jwt = require("jsonwebtoken");
 
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
 
-const db = new Pool({
-    user: "postgres" ,
-    host:"localhost",  
-    database: "Innovation_db", 
-    password: "Lethabo2015!!", 
-    port: "5432"
-  });
 
 exports.register = async (req, res)=>{ 
     console.log(req.body)
